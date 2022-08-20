0eca80eb912926154bd5895da1fbc475af0d772f
SerhiiPavlyk <sergijpavlik@gmail.com>
2022-08-20 11:17:14 +0300

    Lab_01 second commit

--- Lab_01/Hello.java
+++ Lab_01/Hello.java
@@ -1,8 +1,8 @@
-public class Hello
- {
-    public static void main (String[] args)
-    {String message = new String();
-    message = "Hello, Java";
-    System.out.println(message);
+public class Hello {
+    public static void main(String[] args) {
+        String name = "Serhii";
+        String last_name = "Pavlyk";
+        String group = "KI-35";
+        System.out.println(last_name+ " " +name + " "  + group);
     }
 }
