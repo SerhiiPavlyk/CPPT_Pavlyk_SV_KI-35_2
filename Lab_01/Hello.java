438907e86a346a409ea0cd7291238053e77abf50
SerhiiPavlyk <sergijpavlik@gmail.com>
2022-09-28 15:25:14 +0300

    Delete 1 and 2 LABS

--- Lab_01/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        String name = "Serhii";
-        String last_name = "Pavlyk";
-        String group = "KI-35";
-        System.out.println(last_name+ " " +name + " "  + group);
-}
-} 
--- Lab_02/Lab_02 doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Lab_02","l":"Lab_02()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab_02","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
