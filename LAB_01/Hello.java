98d753efb60c7fdf46b1a483553086777ea010c0
SerhiiPavlyk <sergijpavlik@gmail.com>
2022-09-28 15:26:54 +0300

    Signed-off-by: SerhiiPavlyk <sergijpavlik@gmail.com>

---
+++ LAB_01/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = "Serhii";
+        String last_name = "Pavlyk";
+        String group = "KI-35";
+        System.out.println(last_name+ " " +name + " "  + group);
+}
+} 
